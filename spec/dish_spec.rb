a3498c3c347d59c77b0aa912fdb69c46755e31a9
Oliver Delevingne <oliver@delevingne.co.uk>
2014-07-09 13:29:36 +0100

    dishes have a price. tests passing. dish class complete

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,10 +2,14 @@ require 'dish'
 
 describe 'Dish' do
 
-	let(:dish) { Dish.new('Bacon Burger') }
+	let(:dish) { Dish.new('Bacon Burger', '8') }
 	
-	it 'should be created with a name' do
+	it 'is created with a name' do
 		expect(dish.name).to eq 'Bacon Burger'
+	end
+
+	it 'is created with a price' do
+		expect(dish.price).to eq '8'
 	end
 
 
